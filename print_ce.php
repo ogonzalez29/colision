e03845a1c98cb05f56858884dca275b032eb464a
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-08-23 19:21:03 -0500

    added header and client-vehicle info

---
+++ print_ce.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title>Cotización Colisión Exprés</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="grid">
+			<div class="header">
+				<div class="col-12">
+					<img src="img/logo.png" alt="logo servitalleres">
+					<div style="float:right; padding-right:5px;" class="col-4">
+						<div style="float:left;" class="col-6">
+							<h2>Cotización:</h2>	
+						</div>
+						<div style="float:right; text-align:center;" class="col-6">
+							<h2 style="border:1px solid white; color:red; background: white;">N. 649</h2>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div style="border-bottom:1px solid grey;" class="col-12">
+					<h2>SERVICIO DE LATONER&Iacute;A Y PINTURA EXPR&Eacute;S EN CABINA</h2>
+				</div>
+			</div>
+			<div class="row-1">
+				<div class="col-12">
+					<div class="col-1_2">
+						<h3 style="font-weight:bold;">Fecha:</h3>
+					</div>
+					<div class="col-1_3">
+						<h3 style="border-bottom:1px solid black;">23/08/2016</h3>
+					</div>
+					<div class="col-1_2">
+						<h3 style="font-weight:bold;">Nombre:</h3>
+					</div>
+					<div class="col-4">
+						<h3 style="border-bottom:1px solid black;">Oscar Daniel Gonzalez</h3>
+					</div>
+					<div class="col-1_3">
+						<h3 style="font-weight:bold;">Teléfono:</h3>
+					</div>
+					<div class="col-5">
+						<h3 style="border-bottom:1px solid black;">3186515096</h3>
+					</div>
+				</div>
+				<div class="col-12">
+					<div class="col-1_2">
+						<h3 style="font-weight:bold;">Correo:</h3>
+					</div>
+					<div class="col-4">
+						<h3 style="border-bottom:1px solid black;">oscar.d.gonzalez@gmail.com</h3>
+					</div>
+					<div class="col-1_3">
+						<h3 style="font-weight:bold;">Vehículo:</h3>
+					</div>
+					<div class="col-5">
+						<h3>Renault Clio</h3>
+					</div>
+					<div class="col-1_2">
+						<h3 style="font-weight:bold;">Placa:</h3>
+					</div>
+					<div class="col-1_2">
+						<h3>CVL685</h3>
+					</div>
+				</div>
+				<div class="col-12">
+					
+				</div>
+				<div class="col-12">
+					
+				</div>
+				<div class="col-12">
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
