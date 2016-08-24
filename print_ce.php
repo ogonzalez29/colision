fc2f8ca8747924a80bb6a6a13b0887c88ef8f339
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-08-24 16:38:21 -0500

    finished print_ce.php document layout_without php code_

--- print_ce.php
+++ print_ce.php
@@ -10,11 +10,11 @@
 			<div class="header">
 				<div class="col-12">
 					<img src="img/logo.png" alt="logo servitalleres">
-					<div style="float:right; padding-right:5px;" class="col-4">
-						<div style="float:left;" class="col-6">
+					<div style="float:right; padding-right:5px;" class="col-5">
+						<div style="float:left;" class="col-7">
 							<h2>Cotización:</h2>	
 						</div>
-						<div style="float:right; text-align:center;" class="col-6">
+						<div style="float:right; text-align:center;" class="col-7">
 							<h2 style="border:1px solid white; color:red; background: white;">N. 649</h2>
 						</div>
 						
@@ -22,59 +22,128 @@
 				</div>
 			</div>
 			<div class="row">
-				<div style="border-bottom:1px solid grey;" class="col-12">
+				<div style="border-bottom:1px solid #d3d3d3;" class="col-12">
 					<h2>SERVICIO DE LATONER&Iacute;A Y PINTURA EXPR&Eacute;S EN CABINA</h2>
 				</div>
 			</div>
 			<div class="row-1">
-				<div class="col-12">
+				<div style="text-align:center; padding-right: 5px;" class="col-12">
 					<div class="col-1_2">
 						<h3 style="font-weight:bold;">Fecha:</h3>
 					</div>
-					<div class="col-1_3">
+					<div class="col-1_4">
 						<h3 style="border-bottom:1px solid black;">23/08/2016</h3>
 					</div>
+					<div class="col-3">
+						<h3 style="font-weight:bold;">Asesor de servicio:</h3>
+					</div>
+					<div class="col-3">
+						<h3 style="border-bottom:1px solid black;">Daniel Gonzalez</h3>
+					</div>
+				</div>
+				<div style ="padding-right: 5px;" class="col-12">
 					<div class="col-1_2">
 						<h3 style="font-weight:bold;">Nombre:</h3>
 					</div>
-					<div class="col-4">
-						<h3 style="border-bottom:1px solid black;">Oscar Daniel Gonzalez</h3>
+					<div class="col-3">
+						<h3 style="border-bottom:1px solid black;">Oscar Gonzalez</h3>
 					</div>
-					<div class="col-1_3">
+					<div class="col-1_2">
 						<h3 style="font-weight:bold;">Teléfono:</h3>
 					</div>
-					<div class="col-5">
+					<div class="col-1_4">
 						<h3 style="border-bottom:1px solid black;">3186515096</h3>
 					</div>
-				</div>
-				<div class="col-12">
-					<div class="col-1_2">
+					<div class="col-07">
 						<h3 style="font-weight:bold;">Correo:</h3>
 					</div>
-					<div class="col-4">
-						<h3 style="border-bottom:1px solid black;">oscar.d.gonzalez@gmail.com</h3>
+					<div class="col-6">
+						<h3 style="border-bottom:1px solid black;">oscar.d.gonzalez@hotmail.com</h3>
 					</div>
-					<div class="col-1_3">
+				</div>
+				<div style="text-align:center;" class="col-12">
+					<div class="col-1_4">
 						<h3 style="font-weight:bold;">Vehículo:</h3>
 					</div>
-					<div class="col-5">
-						<h3>Renault Clio</h3>
+					<div class="col-4">
+						<h3 style="border-bottom:1px solid black;">Renault Clio</h3>
 					</div>
 					<div class="col-1_2">
 						<h3 style="font-weight:bold;">Placa:</h3>
 					</div>
 					<div class="col-1_2">
-						<h3>CVL685</h3>
+						<h3 style="border-bottom:1px solid black;">CVL685</h3>
+					</div>
+					<div class="col-1_2">
+						<h3 style="font-weight:bold;">Modelo:</h3>
+					</div>
+					<div class="col-07">
+						<h3 style="border-bottom:1px solid black;">2008</h3>
 					</div>
 				</div>
 				<div class="col-12">
-					
+					<div style= "vertical-align:top; height: 68px;" class="col-3">
+						<h3 style="text-align:left; font-weight:bold; margin: 1.5em 0;">Piezas a intervenir:</h3>
+					</div>
+					<div style="text-align:justify; height:68px; line-height:68px;" class="col-9">
+						<span style="border-bottom:1px solid black;">Nulla ac gravida arcu. Nunc blandit, est eget lacinia volutpat, dolor ex tempus arcu, non elementum risus lectus vel lectus. In ultricies enim ut leo consequat, a molestie metus congue. Donec ut phare</span>
+					</div>
 				</div>
 				<div class="col-12">
-					
+					<div style="width:22%;" class="col-3">
+						<h3 style="text-align:left; font-weight:bold;">Repuestos asociados:</h3>
+					</div>
+					<div style="width:77.4%;" class="col-9">
+						<form action="">
+							<input type="radio" name="1" value="Si"> Si
+							<input type="radio" name="1" value="No"> No
+						</form>
+					</div>
+				</div>
+				<div style="display:none;" id="repuestos" class="col-12">
+					<div style= "vertical-align:top;" class="col-3">
+						<h3 style="text-align:left; font-weight:bold;">Repuestos:</h3>
+					</div>
+					<div style="text-align:justify; height:32px; line-height:30px;" class="col-9">
+						<span style="border-bottom:1px solid black;">Nulla ac gravida arcu. Nunc blandit, est eget lacinia volutpat, dolor ex tempus arcu, non</span>
+					</div>
+				</div>
+			</div>
+			<div class="row-2">
+				<div style="text-align:right; padding-right:5px;" class="col-12">
+					<div style="width:36%;" class="col-6">
+						<h2>Su reparación le cuesta:</h2>
+					</div>
+					<div style="border:1px solid black;" class="col-3">
+						<h2>$1.000.000 *</h2>
+					</div>
+				</div>
+				<div style="text-align:right; padding-right:5px;" class="col-12">
+					<div class="col-6">
+						<h2>Tiempo de entrega:</h2>
+					</div>
+					<div style="border:1px solid black;" class="col-3">
+						<h2>48 horas</h2>
+					</div>
+				</div>
+				<div style="text-align:right; padding-right:5px;" class="col-12">
+					<div class="col-4">
+						<h3 style="font-weight:bold;">Validez de la cotización:</h3>
+					</div>
+					<div class="col-07">
+						<h3 style="border-bottom:1px solid black;">8 días</h3>
+					</div>
+					<div class="col-1_4">
+						<h3>* IVA incluido</h3>
+					</div>
+				</div>
+			</div>
+			<div class="footer">
+				<div class="col-12">
+					<h4>Carrera 22 # 76-57 | Bogotá - Colombia | 2117943 - 2119290</h4>
 				</div>
 				<div class="col-12">
-					
+					<h4 style="margin-top:5px;">contacto@servitalleres.com | www.servitalleres.com</h4>
 				</div>
 			</div>
 		</div>
